844083b6722e15b5f20517d414f51189815e1d23
James Roncesvalles <roncy.pro13@gmail.com>
2020-02-08 17:21:41 +0800

    add student file

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php 
   header("Access-Control-Allow-Origin: *");
   header("Content-Type: application/json; charset=UTF-8");
+
+  $user = array("first_name" => "James", "last_name" => "Roncesvalles");
+
+  echo json_encode($user);
 ?>
---
+++ student.php
@@ -0,0 +1,16 @@
+<?php
+
+Class Student {
+
+  public $firstName;
+  public $middleName;
+  public $lastName;
+  public $address;
+
+  private $amount = 0;
+
+  function __construct() {}
+
+}
+
+?>
